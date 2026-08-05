883ad9d0628fdbe5ec8c3f13b57e4de43ff698ab
Elena Blizhenskaya <69386594+Elena1609@users.noreply.github.com>
2026-08-05 15:08:06 +0300

    Time: 8 ms (13.08%), Space: 12.5 MB (28.18%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -9,8 +9,9 @@ class Solution(object):
         wordlist = []
         worddict = {}
         banned = set(banned)
+        delimiters = {" ", "!", "?", "'", ",", ";", "."}
         for each in paragraph:
-            if each not in [" ","!","?","'",",",";","."]:
+            if each not in delimiters:
                 newword = newword + each.lower()
             else:
                 if newword != "":
