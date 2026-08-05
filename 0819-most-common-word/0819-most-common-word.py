d38313b929c656722b1223e4ce05885aded1e1c3
Elena Blizhenskaya <69386594+Elena1609@users.noreply.github.com>
2026-08-05 15:27:09 +0300

    Time: 3 ms (69.8%), Space: 12.3 MB (95.76%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -10,15 +10,16 @@ class Solution(object):
         banned = set(banned)
         delimiters = " !?',;."
         paragraph = paragraph.lower()
+        letters = []
         for each in paragraph + " ":
             if each not in delimiters:
-                newword += each
+                #newword += each
+                letters.append(each)
             else:
+                newword = "".join(letters)
                 if newword != "" and newword not in banned:
                     worddict[newword] = worddict.get(newword, 0) + 1
-                newword = ""     
-        #if newword != "" and newword not in banned:
-        #    worddict[newword] = worddict.get(newword, 0) + 1
-            
+                #newword = ""
+                letters = []                 
         return max(worddict, key=worddict.get)
     
