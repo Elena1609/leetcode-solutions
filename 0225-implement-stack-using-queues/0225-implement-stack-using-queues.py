22e7354a91083c7168111928f492164c72ef619f
Elena Blizhenskaya <69386594+Elena1609@users.noreply.github.com>
2026-08-06 21:49:39 +0300

    Time: 0 ms (100%), Space: 12.4 MB (68.51%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -11,7 +11,7 @@ class MyStack(object):
         :rtype: None
         """
         self.q2.append(x)
-        while self.q1:
+        while len(self.q1) > 0:
             self.q2.append(self.q1.pop(0))
         
         self.q1, self.q2 = self.q2, self.q1    
