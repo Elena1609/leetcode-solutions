4b59182b24cb8fe40a6aa67503939dc4050f206c
Elena Blizhenskaya <69386594+Elena1609@users.noreply.github.com>
2026-08-22 18:24:55 +0300

    Time: 5803 ms (5.03%), Space: 20.7 MB (5.32%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+'''
 class Solution(object):
     def mergeKLists(self, lists):
         """
@@ -28,3 +29,44 @@ class Solution(object):
             current = current.next
 
         return head
+'''
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[Optional[ListNode]]
+        :rtype: Optional[ListNode]
+        """
+        current = []
+        for i in range(len(lists)):
+            current.append(lists[i])
+        
+        result = []
+
+        while any(current):
+
+            for i in range(len(current)):
+                if current[i] is not None:
+                    minimum = current[i].val
+                    index = i
+                    break
+
+            for i in range(len(current)):
+                if current[i] is not None and current[i].val < minimum:
+                    minimum = current[i].val
+                    index = i           
+
+            result.append(minimum)
+            current[index] = current[index].next
+
+        if result:
+            head = ListNode(result[0])
+        else:
+            head = None
+
+        current = head
+
+        for i in range(1, len(result)):
+            current.next = ListNode(result[i])
+            current = current.next
+
+        return head    
